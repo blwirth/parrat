456c599038766195ee1125e0ea7c309fa2aaeeb6
blwirth <127057672+blwirth@users.noreply.github.com>
2025-12-12 07:26:11 -0500

    feat: show preview on csv export

--- epath-to-matchpro/main.ps1
+++ epath-to-matchpro/main.ps1
@@ -11,6 +11,7 @@
 . "$PSScriptRoot\export-selected-xml.ps1"
 . "$PSScriptRoot\export-selected-csv.ps1"
 . "$PSScriptRoot\export-all-csv.ps1"
+. "$PSScriptRoot\export-preview.ps1"
 
 . "$PSScriptRoot\button-handlers\btnOpen.ps1"
 . "$PSScriptRoot\button-handlers\btnXml.ps1"
