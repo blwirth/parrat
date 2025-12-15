9e3cf3bede3ccffd1e3548cd9a62bff5988660ee
blwirth <127057672+blwirth@users.noreply.github.com>
2025-12-15 11:27:16 -0500

    chore: update nav columns
    - Added dateOfBirth and primarySite

--- button-handlers/btnOpen.ps1
+++ button-handlers/btnOpen.ps1
@@ -52,24 +52,30 @@ function Get-BtnOpenHandler {
                     [void]$table.Columns.Add("Index", [int])
                     [void]$table.Columns.Add("nameLast", [string])
                     [void]$table.Columns.Add("nameFirst", [string])
-                    [void]$table.Columns.Add("dateOfDiagnosis", [string])
+					[void]$table.Columns.Add("dateOfBirth", [string])
                     [void]$table.Columns.Add("pathReportNumber1", [string])
+					[void]$table.Columns.Add("primarySite", [string])
+                    [void]$table.Columns.Add("dateOfDiagnosis", [string])
 
                     for ($i = 0; $i -lt $ScriptVars['Tumors'].Count; $i++) {
                         $tumor = $ScriptVars['Tumors'][$i]
                         $patient = $tumor.SelectSingleNode("ancestor::n:Patient[1]", $ScriptVars['NsMgr'])
 
-                        $nameLast = ""
-                        $nameFirst = ""
-                        $dxDate = ""
-                        $pathReportNumber1 = ""
+                        $nameLast 			= ""
+                        $nameFirst 			= ""
+						$dateOfBirth		= ""
+                        $dxDate 			= ""
+                        $pathReportNumber1 	= ""
+						$primarySite		= ""
 
                         if ($patient -ne $null) {
                             $nlNode = $patient.SelectSingleNode("./n:Item[@naaccrId='nameLast']", $ScriptVars['NsMgr'])
                             $nfNode = $patient.SelectSingleNode("./n:Item[@naaccrId='nameFirst']", $ScriptVars['NsMgr'])
+							$dbNode = $patient.SelectSingleNode("./n:Item[@naaccrId='dateOfBirth']", $ScriptVars['NsMgr'])
 
                             if ($nlNode) { $nameLast = $nlNode.InnerText }
                             if ($nfNode) { $nameFirst = $nfNode.InnerText }
+							if ($dbNode) { $dateOfBirth = $dbNode.InnerText }
                         }
 
                         $dxNode = $tumor.SelectSingleNode("./n:Item[@naaccrId='dateOfDiagnosis']", $ScriptVars['NsMgr'])
@@ -77,13 +83,18 @@ function Get-BtnOpenHandler {
                         
                         $pathNode = $tumor.SelectSingleNode("./n:Item[@naaccrId='pathReportNumber1']", $ScriptVars['NsMgr'])
                         if ($pathNode) { $pathReportNumber1 = $pathNode.InnerText }
+						
+						$primaryNode = $tumor.SelectSingleNode("./n:Item[@naaccrId='primarySite']", $ScriptVars['NsMgr'])
+                        if ($primaryNode) { $primarySite = $primaryNode.InnerText }
 
                         $row = $table.NewRow()
                         $row["Selected"] = $false
                         $row["Index"] = $i + 1
                         $row["nameLast"] = $nameLast
                         $row["nameFirst"] = $nameFirst
+						$row["dateOfBirth"] = $dateOfBirth
                         $row["pathReportNumber1"] = $pathReportNumber1
+						$row["primarySite"] = $primarySite
                         $row["dateOfDiagnosis"] = $dxDate
 
                         [void]$table.Rows.Add($row)
@@ -97,8 +108,10 @@ function Get-BtnOpenHandler {
                     $Controls['gridNav'].Columns["Index"].ReadOnly = $true
                     $Controls['gridNav'].Columns["nameLast"].ReadOnly = $true
                     $Controls['gridNav'].Columns["nameFirst"].ReadOnly = $true
-                    $Controls['gridNav'].Columns["dateOfDiagnosis"].ReadOnly = $true
+					$Controls['gridNav'].Columns["dateOfBirth"].ReadOnly = $true
                     $Controls['gridNav'].Columns["pathReportNumber1"].ReadOnly = $true
+                    $Controls['gridNav'].Columns["dateOfDiagnosis"].ReadOnly = $true
+					$Controls['gridNav'].Columns["primarySite"].ReadOnly = $true
                     
                     # Set checkbox column width and move to first position
                     $Controls['gridNav'].Columns["Selected"].Width = 60
