4764ba3850baac536c5e19fb5615c5cf6fb47dee
blwirth <brittneewirth@gmail.com>
2026-01-21 07:33:15 -0500

    chore: rename file

--- parat.ps1
+++ parat.ps1
@@ -59,7 +59,7 @@ Add-Type -AssemblyName System.Drawing
 
 $form = New-Object System.Windows.Forms.Form
 $form = $form[0]  # ensure scalar type, not array
-$form.Text   = "NAACCR XML and HL7 Utilities"
+$form.Text   = "PAthology Reports and cancer Abstract Tools"
 $form.StartPosition = "CenterScreen"
 $form.WindowState   = "Maximized"
 
