05d13217eadcef022c0bdc7e2f0a093685cdfe38
blwirth <127057672+blwirth@users.noreply.github.com>
2026-01-21 08:31:55 -0500

    chore: change filename suffix

--- lib/assign-site-laterality.ps1
+++ lib/assign-site-laterality.ps1
@@ -978,7 +978,7 @@ function Show-AssignmentReport {
         try {
             $originalFileName = [System.IO.Path]::GetFileNameWithoutExtension($OriginalFilePath)
             $directory = [System.IO.Path]::GetDirectoryName($OriginalFilePath)
-            $outputPath = [System.IO.Path]::Combine($directory, "$originalFileName-assigned.xml")
+            $outputPath = [System.IO.Path]::Combine($directory, "$originalFileName-psite.xml")
 
             $nsMgr = New-Object System.Xml.XmlNamespaceManager($XmlDoc.NameTable)
             $nsMgr.AddNamespace("n", $XmlDoc.DocumentElement.NamespaceURI)
