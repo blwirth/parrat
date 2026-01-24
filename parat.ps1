ba267b7edbf07d9de88bdb66e661f3ba0a595b74
blwirth <127057672+blwirth@users.noreply.github.com>
2026-01-24 11:25:18 -0500

    chore: default configs and hide console window

--- parat.ps1
+++ parat.ps1
@@ -1,3 +1,13 @@
+# Hide the PowerShell console window
+Add-Type -Name Window -Namespace Console -MemberDefinition '
+[DllImport("Kernel32.dll")]
+public static extern IntPtr GetConsoleWindow();
+[DllImport("user32.dll")]
+public static extern bool ShowWindow(IntPtr hWnd, Int32 nCmdShow);
+'
+$consolePtr = [Console.Window]::GetConsoleWindow()
+[void][Console.Window]::ShowWindow($consolePtr, 0)  # 0 = SW_HIDE
+
 . "$PSScriptRoot\lib\xml-helpers.ps1"
 . "$PSScriptRoot\lib\xml-viewer.ps1"
 . "$PSScriptRoot\lib\hl7-helpers.ps1"
